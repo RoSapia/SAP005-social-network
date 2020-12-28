0c585e3aaebaedc0e0ce486970eef32bd1989165
RoSapia <rosapia@DESKTOP-BG3EG6H.localdomain>
2020-12-28 15:50:31 -0300

    inserindo scripts firebase

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,7 @@
 export const Login = () => {
-  // Coloque sua página
-  const rootElement = document.createElement('div');
-  rootElement.innerHTML = `
+    // Coloque sua página
+    const rootElement = document.createElement('div');
+    rootElement.innerHTML = `
       <h1>LOGIN</h1>
 
       <form>
@@ -15,5 +15,34 @@ export const Login = () => {
         </fieldset>
       </form>
   `;
-  return rootElement;
+    //const email = rootElement.getElementById("email")
+    //const password = rootElement.getElementById("password")
+
+    firebase.auth().createUserWithEmailAndPassword('a@a.com.br', '123123')
+        .then((user) => {
+            // Signed in
+            // ...
+            console.log("Usuario cadastrado")
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ..
+            console.log(errorMessage)
+        });
+
+
+
+    firebase.auth().signInWithEmailAndPassword('a@a.com.br', '123123')
+        .then((user) => {
+            console.log('login deu certo')
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+        });
+
+
+    return rootElement;
 };
