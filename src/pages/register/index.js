1b2edc1a4219c4d7133576092fde9fbf70babb7d
RoSapia <rosapia@DESKTOP-BG3EG6H.localdomain>
2021-01-01 19:35:59 -0300

    adicionando página de publicações

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,19 +1,20 @@
 export const Register = () => {
 
-  const rootElement = document.createElement('div');
-  const templateRegister = `
-      <form class="register">
-        <label for="name">Nome</label>
-        <input id="user-name" type="text" name="user-name" required>
+  const rootElement = document.createElement('form');
+  rootElement.id='register-user'
+  rootElement.innerHTML =`
+  <h1>REGISTRE_SE</h1>
+        <div class="email-register">
         <label for="email">E-mail</label>
         <input type="email" class="email" name="email" placeholder="Email" autocomplete="on" required>
+        </div>
+        <div class="password-register">
         <label for="password">Senha</label>
         <input class="password" type="password" name="password" minlength="6" required>
-        <label class="repeat-password" for="repeat-password">Confirmar senha</label>
-        <input class="repeat-password" type="password" name="confirm-password" minlength="6" required>
+        </div>
+        <div class="register-btn">
         <button id="register-button" type="submit">Registrar-se</button>
-      </form>
+        </div>
   `;
-  rootElement.innerHTML = templateRegister
   return rootElement;
 };
